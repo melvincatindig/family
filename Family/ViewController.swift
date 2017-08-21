94e9817e1d64e781f94a13953e1863c1d30c54ed
Chris Catindig <catindigmcm@gmail.com>
2017-08-21 09:37:59 +0800

    Initial Commit

---
+++ Family/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Family
+//
+//  Created by Chris Catindig on 21/08/2017.
+//  Copyright © 2017 Chris Catindig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
